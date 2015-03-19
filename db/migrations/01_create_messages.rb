cc084971b8408cef6f8fbf88a82b97a72fc826ab
axton21 <axton_n@yahoo.com>
2015-03-18 23:39:43 -0400

    so tedious,y shotgun didnt work?

--- db/migrations/01_create_messages.rb
+++ db/migrations/01_create_messages.rb
@@ -7,6 +7,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.string :from, :to
+      t.text :content
+      t.timestamps null: false
     end
   end
 end
